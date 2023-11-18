f508672157e03f5d007a954cf81ab6f15936f1d2
Serega <romanovserega354@gmail.com>
2023-11-18 18:37:13 +0300

    добавили домашнее задание

---
+++ D_z/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.Write("Введите числа через запятую: ");
+string? input = Console.ReadLine();
+char splitSymbol = ',';
+
+WriteArray(ParseArray(input, splitSymbol));
+
+int[] ParseArray(string inputNumbers, char split)
+{
+int numbersCount = 1;
+for (int i = 0; i < inputNumbers.Length; i++)
+{
+    if (inputNumbers[i] == split)
+    numbersCount++;
+}
+int[] numbers = new int[numbersCount];
+int numberIndex = 0;
+string subString = "";
+for (int i = 0; i < inputNumbers.Length; i++)
+{
+   if(inputNumbers[i] == split)
+   {
+    numbers[numberIndex++] = Convert.ToInt32(subString);
+    subString = "";
+   }
+   else
+   {
+    subString += input[i];
+   }
+}
+numbers[numberIndex] =  Convert.ToInt32(subString);
+return numbers;
+}
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1,10);
+    }
+}
+
+void WriteArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+       Console.Write(array[i] + " " ); 
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+  Console.Write(message);
+  return Convert.ToInt32(Console.ReadLine());
+}
