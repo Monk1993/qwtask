30e3d95105082bb3a947736876f31660f2ff0073
Serega <romanovserega354@gmail.com>
2023-11-21 14:31:24 +0300

    внесли изменения

--- Program.cs
+++ Program.cs
@@ -1,29 +1,29 @@
-﻿Random randomizer = new Random();
-int size = 5;
-int[] numbers = new int[size];
+﻿//Random randomizer = new Random();
+//int size = 5;
+//int[] numbers = new int[size];
 
-for (int i = 0; i < numbers.Length; i++)
-{
-    numbers [i] = randomizer.Next(1,10);
-}
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    numbers [i] = randomizer.Next(1,10);
+//}
 
-PrintIntArray(numbers);
+//PrintIntArray(numbers);
 
-for (int i = 0; i < numbers.Length / 2; i++)
-{
-    int temp = numbers[i];
-    numbers[i] = numbers[numbers.Length - 1 - i];
-    numbers[numbers.Length - 1 - i] = temp;  
-}
+//for (int i = 0; i < numbers.Length / 2; i++)
+//{
+//    int temp = numbers[i];
+//    numbers[i] = numbers[numbers.Length - 1 - i];
+//    numbers[numbers.Length - 1 - i] = temp;  
+//}
 
-PrintIntArray(numbers);
+//PrintIntArray(numbers);
 
-void PrintIntArray(int[] array)
-{
-for (int i = 0; i < numbers.Length; i++)
-{
-    Console.Write(numbers[i] + " ");
-}
+//void PrintIntArray(int[] array)
+//{
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    Console.Write(numbers[i] + " ");
+//}
 
-Console.WriteLine();
-}
+//Console.WriteLine();
+//}
